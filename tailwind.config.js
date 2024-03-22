d2879b3b1cd930cc0d13521f23fff95f9a7566da
nomccc <nuravivah224@gmail.com>
2024-03-22 16:01:31 +0800

    update: fiksasi detail berita

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,12 +5,15 @@ module.exports = {
     extend: {
       colors: {
         baseColor: "#085D41",
+        baseColor2: "#0E7956",
         accordion: "#10B981",
         base60: "#0D9A6B",
         base70: "#0A7B56",
         base80: "#085C40",
         routebar: "#CFF1E6",
         decorationRoute: "#085D41",
+        instagram: "#DD2B7B",
+        facebook:"#1877F2"
       },
       fontSize: {
         xxs: "10px",
